b3ffda80a4eff2d4ba2dce17f9a2d3a60266ba15
Eshayat <eshayat.wasiu@gmail.com>
2025-06-25 21:19:15 +0600

    fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
         <a href="#gallery">Gallery</a>
         <a href="#reviews">Reviews</a>
         <a href="#about">About</a>
-        <a href="/books">Books</a>
+        <a href="/blog">Blog</a>
       </div>
       <div className="mt-4 text-center">
         <p>
